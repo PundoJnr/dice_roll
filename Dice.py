b70d57dc45d82775d2f72b50d26dcf7fa20df04d
Arthur Pundo <pundoarthur@gmail.com>
2017-09-21 09:18:57 +0300

    improvement
    with if statement and allows for user input

--- Dice.py
+++ Dice.py
@@ -1,8 +1,16 @@
 from random import *
+b = input("Enter 1 to role dice: ")
+if b == "1":
+    die1 = randint(1,6)
+    die2 = randint (1,6)
+    x = (die1 + die2)
+    print (x)
+    if x > 5:
+        print("You're doing great")
+    else :
+        print("oh! bad luck Try again")
+
+else:
+    print("invalid choice")
 
 
-die1 = randint(1,6)
-die2 = randint (1,6)
-n = die1 + die2
-
-print(n)
